26e4f0095ae3f183e9c93676337c02d9626e46e3
akhilakalpuri <70198493+akhilakalpuri@users.noreply.github.com>
2021-02-25 19:55:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+class AnyColorButton extends Component {
+  displayAlert= () => {
+    alert('This is an alert');
+  }
+
+  render() {
+    return (
+      <Button title="Click me" color={this.props.color} onPress={this.displayAlert} />
+    )
+  }
+}
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <View style={{ marginTop: 200 }}>
+        <AnyColorButton color='purple'/>
+        <Text>My First React component</Text>
+      </View>
+    );
+  }
+}
